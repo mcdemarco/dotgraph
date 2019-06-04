fa32f9c7c9dd75e0774468ee9aab302fd1081b6e
M. C. DeMarco <M. C. DeMarco@localhost>
2019-06-03 20:43:21 -0400

    Fix empty dotgraph settings issue.

--- src/index.js
+++ src/index.js
@@ -542,7 +542,7 @@ context.settings = (function () {
 		else 
 			StorySettings = window.document.querySelector('tw-passagedata[name="StorySettings"]');
 
-		if (!StorySettings || !StorySettings.innerText)
+		if (!StorySettings || !StorySettings.innerText || StorySettings.innerText.indexOf("dotgraph:") < 0)
 			return;
 
 		var dgSettings = (StorySettings.innerText.split("dotgraph:")[1]).split("\n")[0];
